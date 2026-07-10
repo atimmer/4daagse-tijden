96c2b770a6256f49e027a1d1ee90de4414ccff41
Anton Timmermans <email@atimmer.com>
2026-07-10 15:54:30 +0200

    Cache app icons for offline use

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,15 @@ export default defineConfig({
     react(),
     tailwindcss(),
     VitePWA({
+      includeAssets: [
+        "android-chrome-192x192.png",
+        "android-chrome-512x512.png",
+        "apple-touch-icon.png",
+        "favicon-16x16.png",
+        "favicon-32x32.png",
+        "favicon.ico",
+        "site.webmanifest",
+      ],
       injectRegister: "script",
       manifest: false,
       registerType: "autoUpdate",
